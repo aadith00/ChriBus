068af9f68d6602dd33722bb27f5759a0007cb8f7
Aadith <maadith2002@gmail.com>
2024-01-07 11:34:31 +0530

    Update

--- base/views.py
+++ base/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
 def signup(request):
+    print(request.method)
+
+##    import pdb;                   This is used to pause the page for sometime and examine the request object so as to check if there are any errors.
+##    pdb.set_trace()
+
+
+
+
     return render (request, "signup.html")
 
 def booking(request):
