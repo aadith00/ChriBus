399704f92b6b0520d63a2bbd2e571c0aa44abe14
VIKAS-006 <vikasviks9@gmail.com>
2024-01-28 15:34:09 +0530

    update

--- rentals/models.py
+++ rentals/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class rent:
-    register_number : int
-    name : str
-    img : str
-    desc : str
-    price : int
+class rent(models.Model):
+    name : models.CharField(max_length=100)
+    price : models.IntegerField
--- rentals/urls.py
+++ rentals/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='rentals'),
-    path('',views.contact,name='support') 
+    path('support/',views.contact,name='support') 
 ]
