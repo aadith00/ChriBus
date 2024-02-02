b40c4950551e8adc6d9690bbdd9838eb677a1863
VIKAS-006 <vikasviks9@gmail.com>
2024-02-02 23:01:25 +0530

    worked on the ui and backend of rentals

--- rentals/urls.py
+++ rentals/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, upload, contact
+from .views import index, upload, contact,register
 
 urlpatterns = [
     path('',index),
-    path('upload/',upload),
+    path('upload',upload),
     path('support/',contact),
+    path('login',register)
 ]
