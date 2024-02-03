596677e7defda1417e1fa6c30c370749a16c7e59
Aadith <maadith2002@gmail.com>
2024-02-03 23:44:15 +0530

    Update

--- bus/admin.py
+++ bus/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from .models import Bus, Booking
 
-class BusAdmin(admin.ModelAdmin):
-    list_display = ('num_plate', 'driver_id', 'departure_date', 'total_seats')
-    search_fields = ('num_plate', 'driver_id', 'departure_date')
-
-admin.site.register(Bus, BusAdmin)
-
-
+admin.site.register(Bus)
 admin.site.register(Booking)
